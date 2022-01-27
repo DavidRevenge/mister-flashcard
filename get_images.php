e839c167f3c156b32725de718c5d085cda094367
Davide Buccella <buccella.davide@gmail.com>
2022-01-27 21:41:59 +0100

    wip

--- get_images.php
+++ get_images.php
@@ -9,7 +9,7 @@ $word = urlencode($word);
 //The URL with parameters / query string.
 //$url = 'https://api.qwant.com/api/search/images?t=image&q=' . $word . '&count=50&uiv=4&locale=it_IT';
 //https://api.qwant.com/v3/search/images?q=dog&t=images&count=50&locale=it_IT&offset=0&device=desktop&safesearch=1
-$url = 'https://api.qwant.com/v3/search/images?q=' . $word . '&t=images&count=50&locale=it_IT&offset=0';
+$url = 'https://api.qwant.com/v3/search/images?q=' . $word . '&t=images&count=200&locale=it_IT&offset=0';
 $curl_handle = curl_init();
 curl_setopt($curl_handle, CURLOPT_URL, $url);
 curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
